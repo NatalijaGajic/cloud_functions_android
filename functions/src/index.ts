61baf33bcfd487fbd7c11c71771abf022000d1d4
Natalija Gajic <nat.gaj98@gmail.com>
2020-04-21 18:39:54 +0200

    createWishlist trigger for notifications

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -26,14 +26,17 @@ exports.updatePost = functions.firestore
             
             const promises: any[] = [];
             const favRef = admin.firestore().collection("wishlist").where("postID","==" ,id);
-            return  favRef.get().then(docs => {
+              favRef.get().then(docs => {
                 docs.forEach(doc => {
                     console.log("jedan wishlist:");
                     console.log(doc.data());
                     const newFavePost = {
                         postID: id,
                         userID: doc.data().userID,
-                        post: newValue
+                        post: newValue,
+                        date: doc.data().date,
+                        time: doc.data().time,
+                        timestamp: doc.data().timestamp
                     };
                     const promise = admin.firestore().collection("wishlist").doc(id+doc.data().userID)
                     .set(newFavePost);
@@ -101,7 +104,146 @@ exports.deletePost = functions.firestore.document('/posts/{id}')
             })
     });
 
-exports.updateMessage = functions.firestore.document('messages/{id}')
-    .onUpdate((change, context) => {
+
+    
+    exports.createWishlist = functions.firestore.document('/wishlist/{id}')
+    .onCreate((snapshot, context) => {
+        const snapshotData = snapshot.data();
+        if(snapshotData != undefined){
+            const userWhoWishlisted = snapshotData.userID;
+            const postID = snapshotData.postID;
+            console.log("User who wishlisted: "+userWhoWishlisted);
+            console.log("Post which is wishlisted: "+postID);
+            const date = snapshotData.date;
+            const time = snapshotData.time;
+            console.log(date + " " + time);
+            let timestamp: Date;
+            timestamp = snapshotData.timestamp;
+            console.log("timestamp "+ timestamp);
+            let post: {date: string, description: string, imageUri: string,
+            postID: string, price: number, title: string, user: {}, userID: string, usersFollowing: [],
+            valute: string };
+            post = snapshotData.post;
+            const userID = post.userID;
+            console.log("userID in post " + userID);
+            return admin.firestore().collection("users").doc(userWhoWishlisted)
+                .get().then(document => {
+                    const userData = document.data();
+                    if(userData!=undefined){
+                        const username = userData.firstName+" "+userData.lastName;
+                        const notification = {
+                            from: userWhoWishlisted,
+                            date: date,
+                            time: time,
+                            timestamp: timestamp,
+                            title: post.title,
+                            username: username,
+                            imageUri: post.imageUri
+                        };
+                       return admin.firestore().collection("users").doc(userID).collection("notifications")
+                        .add(notification);
+                    }
+                    return null;
+                });
+      /*  admin.firestore().collection("users").doc(userID).collection('notifications')
+        .add(notification);*/
+            return null;
+        }
         return null;
+
     });
+
+
+/*
+exports.updateMessages = functions.firestore.document('/MessagesSplitCollection/{id}')
+    .onUpdate((change, context) => {
+        const id: string =  context.params.id;
+        const messages = change.after.data()
+        let array: { date: string, from: string, message: string, time: string, to: string}[];
+        if(messages !== undefined){
+             array = messages.messages;
+             const newMessage = array[array.length-1];
+             const from = array[array.length-1].from;
+             const to = array[array.length-1].to;
+             console.log("to " +to);
+             console.log("from "+ from);
+             //imamo to, treba da odemo u njegov chat i postavimo broj novih poruka
+             //from se postavlja na nulu broj novih
+             //salje se u oba chata, treba da se vidi ciji je post da bi se znalo u kojoj kolekciji je chat
+            //let postUserId: string;
+            let otherUserId: string;
+            let postID: string;
+             if(id.includes(to)){
+                console.log("found postUserID it is from");
+                const index = id.indexOf(to);
+                postID = id.substring(0, index);
+                console.log("postID is "+postID);
+                //postUserId = from;
+                otherUserId = to;
+            } else {
+                console.log("found postUserID it is to");
+                const index = id.indexOf(from);
+                postID = id.substring(0, index);
+                console.log("postID is "+postID);
+                //postUserId = to;
+                otherUserId = from;
+            }
+
+             return admin.firestore().collection('chats').doc(otherUserId).get()
+                .then(documentSnapshot => {
+
+                    let chats: {date: string, imageUri:string, otherUserID: string,
+                    otherUserName: string; postID: string, postTitle: string, postUserID: string,
+                postUserName: string, recentText: { date: string, from: string, 
+                    message: string, time: string, to: string}, users:string[], newMessages: number}[];
+                   
+                    const documentSnapshotData = documentSnapshot.data();
+
+                        if(documentSnapshotData !== undefined){
+                            chats = documentSnapshotData.chats;
+                            let newMessagesTotal = documentSnapshotData.newMessages;
+                            const indexOfChat = chats.findIndex(x => x.postID === postID);
+                            if(indexOfChat !== -1){
+                                console.log("chat in array of chats")
+                                //onda chat vec postoji u nizu chatova
+                                const newChat = chats[indexOfChat];
+                                //ako je poruka od to usera, nema vise novih, u suprotnom plus jedna
+                                if(from === otherUserId){
+                                    newMessagesTotal = newMessagesTotal - newChat.newMessages;
+                                    newChat.newMessages = 0;
+
+                                }else {
+                                    console.log("adding messages");
+                                    newMessagesTotal = newMessagesTotal + 1;
+                                    newChat.newMessages = newChat.newMessages+1;
+                                }
+                                newChat.recentText = newMessage;
+                                const firstSlice = chats.slice(0, indexOfChat);
+                                const secondSlice = chats.slice(indexOfChat+1, chats.length-1);
+                                const newArray = [];
+                                newArray.push(newChat);
+                                const newArrayForDucument = newArray.concat(firstSlice).concat(secondSlice);
+                                
+                                const newDocument = {
+                                    chats: newArrayForDucument,
+                                    newMessages: newMessagesTotal
+                                };
+                                return admin.firestore().collection('chats').doc(otherUserId)
+                                .set(newDocument);
+
+                            }else {
+                                //chat ne postoji u nizu chatova
+                                return null;
+                            }
+                    }else {
+                        //treba napraviti chat
+                        return null;
+                    }
+                })
+            
+             
+        }
+        return null;
+    });*/
+
+
